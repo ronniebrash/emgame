c64e11d2986eba5c8226eb2837141c3ec6b9bd0e
sakuraAK <sakuraAK@users.noreply.github.com>
2016-03-01 00:31:59 -0500

    Update aux.py

--- plugin/resources/lib/aux.py
+++ plugin/resources/lib/aux.py
@@ -3,7 +3,7 @@ __author__ = 'akrutau'
 import logging
 import os.path
 from os.path import expanduser
-import xbmc, xbmcgui
+# import xbmc, xbmcgui
 
 HOME_DIR = expanduser('~')
 LAUNCHERS_DATA_PATH = os.path.join(HOME_DIR, '.kodi/userdata/addon_data/plugin.program.advanced.launcher/launchers.xml')
@@ -12,6 +12,7 @@ LAUNCHERS_PATH = os.path.join(GAME_DATA_PATH, 'launchers')
 THUMBNAILS_PATH = os.path.join(GAME_DATA_PATH, 'thumbnails')
 FORMAT = '[%(asctime)-15s][%(levelname)s][%(funcName)s] - %(message)s'
 
+DEBUG = False
 
 def getLogger(name):
     log_path = os.path.join(GAME_DATA_PATH, 'logs')
@@ -26,27 +27,27 @@ def getLogger(name):
 #get actioncodes from https://github.com/xbmc/xbmc/blob/master/xbmc/guilib/Key.h
 ACTION_PREVIOUS_MENU = 10
 
-class MyClass(xbmcgui.Window):
-  def __init__(self):
-    self.strActionInfo = xbmcgui.ControlLabel(250, 80, 200, 200, '', 'font14', '0xFFBBBBFF')
-    self.addControl(self.strActionInfo)
-    self.strActionInfo.setLabel('Push BACK to quit')
-    self.list = xbmcgui.ControlList(200, 150, 300, 400)
-    self.addControl(self.list)
-    self.list.addItem('English')
-    self.list.addItem('French')
-    self.setFocus(self.list)
-
-  def onAction(self, action):
-    if action == ACTION_PREVIOUS_MENU:
-      self.close()
-
-  def onControl(self, control):
-    if control == self.list:
-      item = self.list.getSelectedItem()
-      self.message('You selected : ' + item.getLabel())
-      self.close()
-
-  def message(self, message):
-    dialog = xbmcgui.Dialog()
-    dialog.ok(" My message title", message)
+# class MyClass(xbmcgui.Window):
+#   def __init__(self):
+#     self.strActionInfo = xbmcgui.ControlLabel(250, 80, 200, 200, '', 'font14', '0xFFBBBBFF')
+#     self.addControl(self.strActionInfo)
+#     self.strActionInfo.setLabel('Push BACK to quit')
+#     self.list = xbmcgui.ControlList(200, 150, 300, 400)
+#     self.addControl(self.list)
+#     self.list.addItem('English')
+#     self.list.addItem('French')
+#     self.setFocus(self.list)
+#
+#   def onAction(self, action):
+#     if action == ACTION_PREVIOUS_MENU:
+#       self.close()
+#
+#   def onControl(self, control):
+#     if control == self.list:
+#       item = self.list.getSelectedItem()
+#       self.message('You selected : ' + item.getLabel())
+#       self.close()
+#
+#   def message(self, message):
+#     dialog = xbmcgui.Dialog()
+#     dialog.ok(" My message title", message)
