fcdfd745054b4a595f001d5968bc665d18c9e254
sakuraAK <sakuraAK@users.noreply.github.com>
2016-03-01 00:31:05 -0500

    Update xml_writer.py

--- plugin/resources/lib/xml_writer.py
+++ plugin/resources/lib/xml_writer.py
@@ -13,13 +13,13 @@ import sys
 import shutil
 import os.path
 from aux import getLogger
-from aux import HOME_DIR, LAUNCHERS_DATA_PATH, THUMBNAILS_PATH, LAUNCHERS_PATH, GAME_DATA_PATH
+from aux import HOME_DIR, LAUNCHERS_DATA_PATH, THUMBNAILS_PATH, LAUNCHERS_PATH, GAME_DATA_PATH, DEBUG
 
 from os.path import expanduser
 
 log = getLogger("launcher.log")
 
-# CATEGORY = 'f8f45febbf5881de8e373f48c8bf81dc'
+CATEGORY = 'f8f45febbf5881de8e373f48c8bf81dc'
 
 
 thumbnails_ext_list = ['.png', '.jpeg', '.jpg']
@@ -29,7 +29,7 @@ cur_launcher_path = ''
 def get_launch_dir(game_name, value):
     str = ''
     path = os.path.join(LAUNCHERS_PATH, value)
-    if os.path.exists(path):
+    if os.path.exists(path) or DEBUG:
         str = path
     else:
         log.error("couldn't find {0}".format(path))
@@ -41,7 +41,7 @@ def get_app_path(game_name, value):
     str = ''
     global cur_launcher_path
     path = os.path.join(cur_launcher_path, value)
-    if os.path.isfile(path):
+    if os.path.isfile(path) or DEBUG:
         str = path
     else:
         log.error("couldn't find {0}".format(path))
@@ -87,30 +87,59 @@ def backup(src, dst):
         retVal = 1
     return retVal
 
-def add_launcher(rowInFile, root, attributes, category_id):
-    launcher = SubElement(root, 'launcher')
-    id = SubElement(launcher, 'id')
-    u = uuid.uuid1()
-    id.text = str(u.hex)
-    finished = SubElement(launcher, 'finished')
-    finished.text = 'false'
-    minimize = SubElement(launcher, 'minimize')
-    minimize.text = 'false'
-    cat = SubElement(launcher, 'category')
-    cat.text = category_id
-    set_attributes(rowInFile, launcher, attributes)
+def add_launcher(rowInFile, attributes, fin):
+        fin.write('<launcher>\n')
+        u = uuid.uuid1()
+        fin.write('<id>{0}</id>\n'.format(str(u.hex)))
+        fin.write('<finished>false</finished>\n')
+        fin.write('<minimize>false</minimize>\n')
+        fin.write('<category>{0}</category>\n'.format(CATEGORY))
+        set_attributes(rowInFile, fin, attributes)
+        fin.write('</launcher>\n')
+    # 	 <launcher>
+		# 		 <id>c685f57cdd6311e5967e4fe1a7ea7b1c</id>
+		# 		 <finished>false</finished>
+		# 		 <minimize>false</minimize>
+		# 		 <category>f8f45febbf5881de8e373f48c8bf81dc</category>
+		# 		 <!--Generated element for supermeatboy-->
+		# 		 <name>SuperMeatBoy</name>
+		# 		 <launcherdir>/home/akrutau/game.data/launchers/supermeatboy</launcherdir>
+		# 		 <application>/home/akrutau/game.data/launchers/supermeatboy/SuperMeatBoy</application>
+		# 		 <args />
+		# 		 <rompath />
+		# 		 <tumbpath />
+		# 		 <fanartpath />
+		# 		 <trailerpath />
+		# 		 <custompath />
+		# 		 <romext />
+		# 		 <platform>Linux</platform>
+		# 		 <thumb />
+		# 		 <fanart />
+		# 		 <genre>Platform</genre>
+		# 		 <release />
+		# 		 <publisher>Team Meat</publisher>
+		# 		 <launcherplot>Super Meat Boy is a platform game in which players control a small, dark red, cube-shaped character named Meat Boy, who must save his cube-shaped, heavily bandaged girlfriend Bandage Girl from the evil scientist Dr. Fetus. The game is divided into chapters, which together contain over 300 levels.[2][3] Players attempt to reach the end of each level, represented by Bandage Girl, while avoiding crumbling blocks, saw blades, and various other fatal obstacles. The player can jump and run on platforms, and can jump off or slide down walls. The core gameplay requires fine control and split-second timing, and has been compared to, regarding both gameplay and level of difficulty, traditional platform games such as Super Mario Bros. and Ghosts'n Goblins</launcherplot>
+		# 		 <lnk />
+		# 		 <roms />
+		# 	</launcher>
 
-def set_attributes(rowInFile,root, attributes):
-    game_name = rowInFile[0]
-    root.append(Comment('Generated element for {0}'.format(game_name)))
+
+
+def set_attributes(rowInFile,fin, attributes):
+    game_name = rowInFile[1]
+    fin.write('<!--Generated element for {0}-->'.format(game_name))
     cnt = 0
+    attr_text = ''
     for attr in attributes:
-        subEl = SubElement(root, attributes[cnt])
-        if attributes[cnt] in auto_gen_attr:
-            subEl.text = auto_gen_attr[attributes[cnt]](game_name, rowInFile[cnt])
+        attr_text = ''
+        if attr in auto_gen_attr:
+            attr_text = auto_gen_attr[attr](game_name, rowInFile[cnt])
         else:
-            subEl.text = rowInFile[cnt]
+            attr_text = rowInFile[cnt]
+        fin.write('<{0}>{1}</{0}>\n'.format(attr, attr_text))
         cnt += 1
+
+
 
 def find_child_val(root, tag):
     for child in root:
@@ -119,37 +148,48 @@ def find_child_val(root, tag):
         else:
             return find_child_val(child, tag)
 
-def run_import(xml_path, csv_path):
-    #create backup
-    backup_path = xml_path + '.bckp'
-    if backup(xml_path, backup_path) != 0:
-        return
-    #start import
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-    for child in root:
-        if child.tag == 'categories':
-            category_id = find_child_val(child, 'id')
-            if category_id == 0:
-                log.error("unable to find games category")
-                return
-        if child.tag == 'launchers':
-            root.remove(child)
-            root = SubElement(root, 'launchers')
-    try:
+def add_launchers(fin, csv_path):
         cnt = 0
         with open(csv_path, 'rb') as f:
             reader = csv.reader(f)
             headers = next(reader, None)
             if headers:
                 for row in reader:
-                    add_launcher(row, root, headers, category_id)
+                    add_launcher(row, headers, fin)
                     #set_attributes(row, root)
                     cnt += 1
                 log.info("{0} rows were imported".format(cnt))
-                tree.write(xml_path)
+                # tree.write(xml_path)
             else:
                 log.info("cannot import data. header row is missing")
+
+
+def run_import(xml_path, csv_path):
+    #create backup
+    backup_path = xml_path + '.bckp'
+    if backup(xml_path, backup_path) != 0:
+        return
+    #start import
+    os.remove(xml_path)
+    try:
+        with open(xml_path, 'a') as fin:
+            fin.write('<advanced_launcher version="1.0">\n')
+            fin.write('<categories>\n')
+            fin.write('<category>\n')
+            fin.write('<id>{0}</id>\n'.format(CATEGORY))
+            fin.write('<name>Games</name>\n')
+            fin.write('<thumb />\n')
+            fin.write('<fanart />\n')
+            fin.write('<genre />\n')
+            fin.write('<description />\n')
+            fin.write('<finished>false</finished>\n')
+            fin.write('</category>\n')
+            fin.write('</categories>\n')
+            fin.write('<launchers>\n')
+            add_launchers(fin, csv_path)
+            fin.write('</launchers>\n')
+            fin.write('</advanced_launcher>')
+            fin.close()
     except IOError as e:
         log.error("I/O error({0}): {1}".format(e.errno, e.strerror))
         backup(backup_path, xml_path)
@@ -159,7 +199,21 @@ def run_import(xml_path, csv_path):
             #print(prettify(orig_root))
     else:
         #cleanup
-        f.close
+        fin.close
+
+
+    # tree = ET.parse(xml_path)
+    # root = tree.getroot()
+    # for child in root:
+    #     if child.tag == 'categories':
+    #         category_id = find_child_val(child, 'id')
+    #         if category_id == 0:
+    #             log.error("unable to find games category")
+    #             return
+    #     if child.tag == 'launchers':
+    #         root.remove(child)
+    #         root = SubElement(root, 'launchers')
+
 
 def main(argv):
    inputfile = ''
@@ -167,6 +221,8 @@ def main(argv):
    if len(argv) < 2:
        #get home directory
        # home_dir = expanduser('~')
+       # inputfile = os.path.join(home_dir, 'emgame/copy_of_working_csv.csv')
+       # outputfile = os.path.join(home_dir, 'emgame/launchers.xml')
        inputfile = os.path.join(GAME_DATA_PATH, 'game_info.csv')
        outputfile = LAUNCHERS_DATA_PATH
    else:
